3c3c47aed4edbc8516f984c72a5a90c47046e1ee
Mani <imaninesan@gmail.com>
2025-05-18 17:03:22 +0800

    chore(api): remove redundant url patterns

--- url_shortener/urls_redirect.py
+++ url_shortener/urls_redirect.py
@@ -1,13 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns_api = [
-    path("<str:version>/shorten/", views.ShortenURLView.as_view(), name="shorten-url"),
-    path("<str:version>/stats/<str:short_code>/", views.ShortURLStatsView.as_view(), name="short-url-stats"),
-]
-
-urlpatterns_redirect = [
+urlpatterns = [
     path("short/<str:short_code>/", views.RedirectShortURLView.as_view(), name="redirect-short-url"),
 ]
-
-urlpatterns = urlpatterns_api + urlpatterns_redirect
