de149c72e1f1dd19e2e2b8c6c64e3ff2a687d17b
Mani <imaninesan@gmail.com>
2025-05-18 16:23:35 +0800

    feat(models):implement logic inside ShortURL model

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -1,3 +1,5 @@
+import random
+import string
 from django.db import models
 
 class ShortURL(models.Model):
@@ -9,3 +11,37 @@ class ShortURL(models.Model):
 
     def __str__(self):
         return f"{self.short_alias} -> {self.original_url}"
+    
+    def increment_access_count(self):
+        self.access_count += 1
+        self.save(update_fields=['access_count'])
+    
+    def deactivate(self):
+        self.is_active = False
+        self.save(update_fields=['is_active'])
+        
+    def activate(self):
+        self.is_active = True
+        self.save(update_fields=['is_active'])
+    
+    @classmethod
+    def generate_short_alias(cls, length=8, version='v1'):
+        strategies = {
+            'v1': cls._algorithm_v1,
+        }
+        if version not in strategies:
+            raise NotImplementedError(f"Short Code generation for version '{version}' is not implemented.")
+        return strategies[version](length)
+
+    
+    @staticmethod
+    def _algorithm_v1(length):
+        # just a simple random string generation for now
+        # security and collision handling can be improved later
+        chars = string.digits + string.ascii_lowercase + string.ascii_uppercase
+        while True:
+            alias = ''.join(random.choices(chars, k=length))
+            if not ShortURL.objects.filter(short_alias=alias).exists():
+                return alias
+        
+    
